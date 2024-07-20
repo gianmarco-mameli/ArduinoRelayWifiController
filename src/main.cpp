dd60d7642942312d6a6fca410818bf26a22ee98e
Gianmarco Mameli <gnammyx@gmail.com>
2024-07-20 16:02:32 +0200

    moved to uno r4 wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,46 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
-#include <WiFiEspAT.h>
-#include <PubSubClient.h>
+// #include <WiFiEspAT.h>
+#include <WiFiS3.h>
+#include <ArduinoMqttClient.h>
 #include <LiquidCrystal_I2C.h>
 
 #include <private.h>
-const char ssid[] = SECRET_SSID;
-const char pass[] = SECRET_PASS;
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
 
+const long interval = 1000;
 unsigned long previousMillis = 0;
-// bool backlightState = false;
-// unsigned long backlightStartTime = 0;
 uint8_t counter = 0;
 
-const char *t_temperature = "modul/temperature";
-float temperature;
-
-const char *t_humidity = "modul/humidity";
-uint8_t humidity;
-
-// const char *t_motion = "veranda/motion";
-// bool motion = 0;
-
-const char *t_pump5 = "rpiext/pump/5";
-const char *t_pump6 = "rpiext/pump/6";
-const char *t_pump7 = "rpiext/pump/7";
-const char *t_pump8 = "rpiext/pump/8";
-
-bool pump5 = 0;
-bool pump6 = 0;
-bool pump7 = 0;
-bool pump8 = 0;
+const char broker[] = "192.168.31.100";
+unsigned int port = 1883;
+const char t_pump5[] = "pump/5";
+const char t_pump6[] = "pump/6";
+const char t_pump7[] = "pump/7";
+const char t_pump8[] = "pump/8";
+const char willTopic[] = "rpiext_extrarelays/status";
+int pump5 = 0;
+char pump6 = 0;
+char pump7 = 0;
+char pump8 = 0;
+#define relay1 PIN_D7
+#define relay2 PIN_D6
+#define relay3 PIN_D5
+#define relay4 PIN_D4
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 char line0[17] = "";
 char line1[17] = "";
 
-#define relay1 PCINT23
-#define relay2 PCINT22
-#define relay3 PCINT21
-#define relay4 PCINT20
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
 
 SoftwareSerial soft(13, 12); // RX, TX
 
 void print_pump(uint8_t pump_number)
 {
+  lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.clear();
   lcd.print(F("Pump ON #"));
@@ -55,19 +48,19 @@ void print_pump(uint8_t pump_number)
   counter = 0;
 }
 
-void callback(char *topic, byte *payload, unsigned int length)
+void onMqttMessage(int messageSize)
 {
-  char in_message[5];
-  unsigned int i = 0;
-  for (; i < length; i++)
-  {
-    in_message[i] = char(payload[i]);
+  const char *topic = mqttClient.messageTopic().c_str();
+  char message;
+  Serial.print(topic);
+  Serial.print(" ");
+  while (mqttClient.available()) {
+    message = (char)mqttClient.read();
   }
-  in_message[i] = '\0';
-  // motion = 0;
   if (strcmp(topic, t_pump5) == 0)
   {
-    pump5 = atoi(in_message);
+    pump5 = (int)message - 48;
+    Serial.println(pump5);
     digitalWrite(relay1, pump5);
     if (pump5)
     {
@@ -76,7 +69,8 @@ void callback(char *topic, byte *payload, unsigned int length)
   }
   if (strcmp(topic, t_pump6) == 0)
   {
-    pump6 = atoi(in_message);
+    pump6 = (int)message - 48;
+    Serial.println(pump6);
     digitalWrite(relay2, pump6);
     if (pump6)
     {
@@ -85,7 +79,8 @@ void callback(char *topic, byte *payload, unsigned int length)
   }
   if (strcmp(topic, t_pump7) == 0)
   {
-    pump7 = atoi(in_message);
+    pump7 = (int)message - 48;
+    Serial.println(pump7);
     digitalWrite(relay3, pump7);
     if (pump7)
     {
@@ -94,91 +89,14 @@ void callback(char *topic, byte *payload, unsigned int length)
   }
   if (strcmp(topic, t_pump8) == 0)
   {
-    pump8 = atoi(in_message);
+    pump8 = (int)message - 48;
+    Serial.println(pump8);
     digitalWrite(relay4, pump8);
     if (pump8)
     {
       print_pump(8);
     }
   }
-  // if (strcmp(topic, t_motion) == 0)
-  // {
-  //   motion = bool(in_message);
-  // }
-  if (strcmp(topic, t_temperature) == 0)
-  {
-    temperature = atof(in_message);
-  }
-  if (strcmp(topic, t_humidity) == 0)
-  {
-    humidity = atoi(in_message);
-  }
-}
-
-void InitWiFi()
-{
-  soft.begin(9600);
-  delay(500);
-  WiFi.init(&soft);
-  if (WiFi.status() == WL_NO_SHIELD)
-  {
-    Serial.println(F("Communication with WiFi module failed!"));
-    while (true)
-      ;
-  }
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(F("Attempting to connect to WPA SSID: "));
-    Serial.println(ssid);
-    uint8_t status = WiFi.begin(ssid, pass);
-    Serial.println();
-
-    if (status != WL_CONNECTED)
-    {
-      Serial.println(F("Failed to connect to AP"));
-    }
-    else
-    {
-      Serial.println(F("You're connected to the network"));
-      Serial.println(WiFi.localIP());
-    }
-  }
-}
-
-void reconnect()
-{
-  while (!client.connected())
-  {
-    if (client.connect("rpiext_extrarelays"))
-    {
-      Serial.println(F("MQTT broker connected"));
-      client.publish(t_pump5, "0");
-      client.publish(t_pump6, "0");
-      client.publish(t_pump7, "0");
-      client.publish(t_pump8, "0");
-      client.subscribe(t_temperature);
-      client.subscribe(t_humidity);
-      // client.subscribe(t_motion);
-      client.subscribe(t_pump5);
-      client.subscribe(t_pump6);
-      client.subscribe(t_pump7);
-      client.subscribe(t_pump8);
-    }
-    else
-    {
-      Serial.print(F("failed with state "));
-      Serial.print(client.state());
-      delay(2000);
-      InitWiFi();
-    }
-  }
-}
-
-void InitMqtt()
-{
-  client.setServer("192.168.1.1", 1883);
-  client.setCallback(callback);
-  reconnect();
 }
 
 void setup()
@@ -196,75 +114,110 @@ void setup()
   digitalWrite(relay3, 0);
   digitalWrite(relay4, 0);
 
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
   lcd.init();
   lcd.backlight();
-  // backlightState = true;
-  // backlightStartTime = millis();
 
-  Serial.begin(9600);
+  String willPayload = "disconnected";
 
-  delay(1000);
-  InitWiFi();
-  delay(1000);
-  InitMqtt();
+  mqttClient.setId("rpiext_extrarelays");
+  mqttClient.beginWill(willTopic, willPayload.length(), false, 1);
+  mqttClient.print(willPayload);
+  mqttClient.endWill();
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  mqttClient.onMessage(onMqttMessage);
+  String payload = "0";
+  unsigned int subscribeQos = 1;
+
+  mqttClient.beginMessage(t_pump5, payload.length(), false, 1, false);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+  mqttClient.beginMessage(t_pump6, payload.length(), false, 1, false);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+  mqttClient.beginMessage(t_pump7, payload.length(), false, 1, false);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+  mqttClient.beginMessage(t_pump8, payload.length(), false, 1, false);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+
+  digitalWrite(relay1, 0);
+  digitalWrite(relay2, 0);
+  digitalWrite(relay3, 0);
+  digitalWrite(relay4, 0);
+
+  mqttClient.subscribe(t_pump5, subscribeQos);
+  mqttClient.subscribe(t_pump6, subscribeQos);
+  mqttClient.subscribe(t_pump7, subscribeQos);
+  mqttClient.subscribe(t_pump8, subscribeQos);
 }
 
-void updateDisplay()
-{
-  lcd.setCursor(0, 0);
-  lcd.print(line0);
-  lcd.setCursor(0, 1);
-  lcd.print(line1);
-}
+// void updateDisplay()
+// {
+//   lcd.setCursor(0, 0);
+//   lcd.print(line0);
+//   lcd.setCursor(0, 1);
+//   lcd.print(line1);
+// }
 
 void loop()
 {
-  unsigned long currentMillis = millis();
+  mqttClient.poll();
 
-  if (currentMillis - previousMillis >= 500)
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
   {
     previousMillis = currentMillis;
-
-    if (!client.connected())
-    {
-      reconnect();
-    }
-    client.loop();
-
-    // if (motion)
-    // {
-    //   lcd.backlight();
-    //   backlightState = true;
-    //   backlightStartTime = millis();
-    //   Serial.println(F("BL"));
-    //   client.publish("rpiext_extrarelays/status", "on");
-    // }
     if (pump5 || pump6 || pump7 || pump8)
     {
-      // lcd.backlight();
-      // backlightState = true;
-      // backlightStartTime = millis();
+      lcd.backlight();
       counter++;
       lcd.setCursor(0, 1);
       lcd.print(counter);
       lcd.print(F(" seconds on"));
-      client.publish("rpiext_extrarelays/status", "pump_on");
+      String payload = "pump_on";
+      mqttClient.beginMessage(willTopic, payload.length(), false, 1, false);
+      mqttClient.print(payload);
+      mqttClient.endMessage();
+      Serial.println("pump_on");
     }
-    // if (backlightState == true && millis() - backlightStartTime >= 8000 && (!pump5 && !pump6 && !pump7 && !pump8))
-    // {
-    //   backlightState = false;
-    //   lcd.noBacklight();
-    //   Serial.println(F("NO BL"));
-    //   client.publish("rpiext_extrarelays/status", "off");
-    // }
     if (!pump5 && !pump6 && !pump7 && !pump8)
     {
-      char tBuffer[20];
-      dtostrf(temperature, 2, 1, tBuffer);
-      sprintf(line0, "Ext Temp: %5s%c", tBuffer, char(223));
-      sprintf(line1, "Ext Hum: %6d%%", humidity);
-      updateDisplay();
-      client.publish("rpiext_extrarelays/status", "connected");
+      lcd.init();
+      lcd.noBacklight();
+      String payload = "connected";
+      mqttClient.beginMessage(willTopic, payload.length(), false, 1, false);
+      mqttClient.print(payload);
+      mqttClient.endMessage();
+      Serial.println("connected");
     }
   }
 }
